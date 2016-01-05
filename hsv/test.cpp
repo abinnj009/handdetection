65902a1555a6bccb4a482cdd327e25f18b0e2735
Abin James <abinnj009@users.noreply.github.com>
2016-01-05 13:55:27 +0530

    Update test.cpp

--- hsv/test.cpp
+++ hsv/test.cpp
@@ -2,7 +2,9 @@
  * @file compareHist_Demo.cpp
  * @brief Sample code to use the function compareHist
  * @author OpenCV team
+ * http://www.teuniz.net/RS-232/ - serial communication
  */
+ 
 
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
